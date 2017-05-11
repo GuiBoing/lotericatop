6cfbe9b2730e4cc91b84831c07030982929c232c
GuiBoing <guilhermeboing@hotmail.com>
2017-05-11 11:08:23 -0300

    Update boing.php

--- boing.php
+++ boing.php
@@ -1,6 +1,14 @@
 <?php
 
-$numeros = rand(1,3);
-print_r($numeros);
-$organiza = asort($numeros);
-print_r ($organiza);
+$jooj=array(1,2);
+
+//function alvorada($jooj){
+
+$array = array($jooj);
+
+$cresce = sort($array);
+
+print_r($cresce);
+/* nao funciona essa porra*/
+
+
